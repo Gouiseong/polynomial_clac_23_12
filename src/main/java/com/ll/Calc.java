4c789d288d01d7b9a5df60e6220cca6dde1f6154
Gouiseong <rhdmltjd1324@gmail.com>
2023-12-26 17:51:22 +0900

    하나의 함수로 여사칙연산 구현 해보기

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,11 +3,21 @@ package com.ll;
 public class Calc {
   public static int run(String exp) {
 
-    String[] bits = exp.split(" \\+ ");
+    int sum;
+    String[] bits=exp.split(" \\+");
+    if(bits.length==2){
+      int a = Integer.parseInt(bits[0]);
+      int b = Integer.parseInt(bits[1]);
+      sum=a+b;
+    }
 
-    int a = Integer.parseInt(bits[0]);
-    int b = Integer.parseInt(bits[1]);
+    String[] bit1=exp.split("-");
+    if(bit1.length==2){
+      int a = Integer.parseInt(bit1[0]);
+      int b = Integer.parseInt(bit1[1]);
+      sum=a-b;
+    }
 
-    return a + b;
+    return sum;
   }
 }
