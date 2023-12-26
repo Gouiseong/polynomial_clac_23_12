dd2a4c9a3696457ac77a6356e90ef05ebbf6fa24
Gouiseong <rhdmltjd1324@gmail.com>
2023-12-26 17:24:04 +0900

    더하기 구í기능 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,17 +2,12 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
-//    String[] a = exp.split("\\+");
-//    int x = Integer.parseInt(a[0]);
-//    int y = Integer.parseInt(a[1]);
-    if (exp.equals("2 + 1")) {
-      return 3;
-    } else if (exp.equals("2 + 2")) {
-      return 4;
-    }
 
+    String[] bits = exp.split(" \\+ ");
 
-    return 2;
-//    return x + y;
+    int a = Integer.parseInt(bits[0]);
+    int b = Integer.parseInt(bits[1]);
+
+    return a + b;
   }
 }
